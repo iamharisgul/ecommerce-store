48c240e1117a75b5bb770701b5a26df6fe07f64b
ghayoor <ghayoor@tncitgroup.com>
2023-07-27 00:11:29 +0400

    confluent kafka

--- store_custom/apps.py
+++ store_custom/apps.py
@@ -5,17 +5,52 @@ class StoreCustomConfig(AppConfig):
     name = 'store_custom'
 
     def ready(self):
-        def read_ccloud_config(config_file):
-            conf = {}
-            with open(config_file) as fh:
-                for line in fh:
-                    line = line.strip()
-                    if len(line) != 0 and line[0] != "#":
-                        parameter, value = line.strip().split('=', 1)
-                        conf[parameter] = value.strip()
-            return conf
+        # !/usr/bin/env python
 
+        import sys
+        from random import choice
+        from argparse import ArgumentParser, FileType
+        from configparser import ConfigParser
         from confluent_kafka import Producer
-        producer = Producer(read_ccloud_config("client.properties"))
-        producer.produce("my-topic", key="key", value="value")
+
+
+        # Parse the command line.
+        parser = ArgumentParser()
+        parser.add_argument('config_file', type=FileType('r'))
+        args = parser.parse_args()
+
+        # Parse the configuration.
+        # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+        config_parser = ConfigParser()
+        config_parser.read_file(args.config_file)
+        config = dict(config_parser['default'])
+
+        # Create Producer instance
+        producer = Producer(config)
+
+        # Optional per-message delivery callback (triggered by poll() or flush())
+        # when a message has been successfully delivered or permanently
+        # failed delivery (after retries).
+        def delivery_callback(err, msg):
+            if err:
+                print('ERROR: Message failed delivery: {}'.format(err))
+            else:
+                print("Produced event to topic {topic}: key = {key:12} value = {value:12}".format(
+                    topic=msg.topic(), key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8')))
+
+        # Produce data by selecting random values from these lists.
+        topic = "purchases"
+        user_ids = ['eabara', 'jsmith', 'sgarcia', 'jbernard', 'htanaka', 'awalther']
+        products = ['book', 'alarm clock', 't-shirts', 'gift card', 'batteries']
+
+        count = 0
+        for _ in range(10):
+            user_id = choice(user_ids)
+            product = choice(products)
+            producer.produce(topic, product, user_id, callback=delivery_callback)
+            count += 1
+
+        # Block until the messages are sent.
+        producer.poll(10000)
+        producer.flush()
 
